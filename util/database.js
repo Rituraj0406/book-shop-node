8f88e7abc31a77575315cb86628e0c86278f6b2b
Rituraj0406 <riturajpiku01@gmail.com>
2025-10-05 22:40:47 +0530

    removed commented code

--- util/database.js
+++ util/database.js
@@ -1,17 +1,6 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-// const mysql = require('mysql2');
-
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'node-complete',
-//     password: 'rootuser'
-// });
-
-// module.exports = pool.promise();
-
 const sequelize = new Sequelize(
   process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
